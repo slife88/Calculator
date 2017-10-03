bf804975ef120b50fdbe5b7b04633a0cb0e81fb5
RTC01 <rtc01@rtc01de-imac.local>
2017-10-03 12:39:35 +0800

    Calculator commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,15 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var history: UILabel!
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var decimalSeparator: UIButton!
+    
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        
+    }
+    @IBAction func floatingPoint(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func backSpace(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 
 }
